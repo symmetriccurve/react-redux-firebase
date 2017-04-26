04e49f0e77c3076832f64b72138fa99fe3239961
Scott Prue <scott@residenetwork.com>
2017-04-26 08:57:06 -0700

    update book

---
+++ examples/complete/material/src/constants/paths.js
@@ -0,0 +1,13 @@
+export const LIST_PATH = '/projects'
+export const ACCOUNT_PATH = '/account'
+export const LOGIN_PATH = '/login'
+export const SIGNUP_PATH = '/signup'
+export const RECOVER_PATH = '/recover'
+
+export default {
+  LIST_PATH,
+  ACCOUNT_PATH,
+  LOGIN_PATH,
+  SIGNUP_PATH,
+  RECOVER_PATH
+}
